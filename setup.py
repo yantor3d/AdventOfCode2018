2a6f0c0bf6c646b86cf72f407523ffdc1459d053
Ryan Porter <ryan.m.porter@gmail.com>
2018-12-01 13:42:53 -0800

    Setup environment

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='Advent of Code 2018',
+    version='0.0.1',
+    description='Solutions to the 2018 Advent of Code programming challenge',
+    author='Ryan Porter',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Other Audience',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='',
+    packages=['aoc'],
+    install_requires=['']
+)
