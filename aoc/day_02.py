090a131d7b6046d0f6e27b6eea1c8f33c8f0efd5
Ryan Porter <ryan.m.porter@gmail.com>
2018-12-02 12:16:51 -0800

    Add day two part two solution

--- aoc/day_02.py
+++ aoc/day_02.py
@@ -1,5 +1,9 @@
+"""Advent of Code 2018 day 02 - https://adventofcode.com/2018/day/2"""
+
 import collections
 import functools
+import itertools
+import operator
 
 import aoc.util 
 
@@ -42,11 +46,50 @@ def calculate_labels_checksum(labels):
     return checksum 
 
 
+def number_of_different_letters(a, b):
+    """Return the number of difference between letters in the same position.
+    
+    Args:
+        a (str): A string of letters
+        b (str0: A string of letters
+
+    Returns:
+        int 
+
+    """
+
+    return sum(map(int, itertools.starmap(operator.ne, zip(a, b))))
+
+
+def find_letters_in_common(a, b):
+    """Return the letters in common between the two strings.
+    
+    Args:
+        a (str): A string of letters
+        b (str0: A string of letters
+
+    Returns:
+        str 
+
+    """
+
+    letters = [x for x, y in zip(a, b) if x == y]
+    return ''.join(letters)
+
+
 def main():
     input_ = aoc.util.get_puzzle_input(2)
 
     answer_01 = calculate_labels_checksum(input_)
     print(f"Part One: {answer_01}")
+
+    pairs_of_labels = itertools.combinations(input_, 2)
+    
+    differs_by_exactly_one_letter = lambda x: number_of_different_letters(*x) == 1
+    labels_that_differ_by_one_letter = filter(differs_by_exactly_one_letter, pairs_of_labels)
+    letters_in_common = itertools.starmap(find_letters_in_common, labels_that_differ_by_one_letter)
+    answer_02 = next(letters_in_common)
+    print(f"Part Two: {answer_02}")
 
 
 if __name__ == '__main__':
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,8 +1,9 @@
+import itertools
 import pytest
 
 import aoc.day_02
 
-TEST_INPUT_DATA = (
+TEST_INPUT_01_DATA = (
     'abcdef',
     'bababc',
     'abbcde',
@@ -12,12 +13,12 @@ TEST_INPUT_DATA = (
     'ababab',
 )
 
-TEST_INPUT_RESULTS_2 = (0, 1, 1, 0, 1, 1, 0)
-TEST_INPUT_RESULTS_3 = (0, 1, 0, 1, 0, 0, 1)
+TEST_INPUT_01_RESULTS_2 = (0, 1, 1, 0, 1, 1, 0)
+TEST_INPUT_01_RESULTS_3 = (0, 1, 0, 1, 0, 0, 1)
 
 
-@pytest.mark.parametrize("input_,expected", zip(TEST_INPUT_DATA, TEST_INPUT_RESULTS_2))
-def test_label_has_exactly_two_letters(input_, expected):
+@pytest.mark.parametrize("input_,expected", zip(TEST_INPUT_01_DATA, TEST_INPUT_01_RESULTS_2))
+def test_01_label_has_exactly_two_letters(input_, expected):
     """Test that a labels with exactly two of any letter return True."""
 
     actual = aoc.day_02.has_exactly_n_letters(input_, 2)
@@ -25,8 +26,8 @@ def test_label_has_exactly_two_letters(input_, expected):
     assert actual == bool(expected)
 
 
-@pytest.mark.parametrize("input_,expected", zip(TEST_INPUT_DATA, TEST_INPUT_RESULTS_3))
-def test_label_has_exactly_three_letters(input_, expected):
+@pytest.mark.parametrize("input_,expected", zip(TEST_INPUT_01_DATA, TEST_INPUT_01_RESULTS_3))
+def test_01_label_has_exactly_three_letters(input_, expected):
     """Test that a labels with exactly three of any letter return True."""
 
     actual = aoc.day_02.has_exactly_n_letters(input_, 3)
@@ -34,9 +35,45 @@ def test_label_has_exactly_three_letters(input_, expected):
     assert actual == bool(expected)
 
 
-def test_label_checksum():
+def test_01_label_checksum():
     """Test that the checksum of the labels is 12."""
 
-    checksum = aoc.day_02.calculate_labels_checksum(TEST_INPUT_DATA)
+    checksum = aoc.day_02.calculate_labels_checksum(TEST_INPUT_01_DATA)
 
     assert checksum == 12
+
+
+TEST_INPUT_02_DATA = [
+    'abcde',
+    'fghij',
+    'klmno',
+    'pqrst',
+    'fguij',
+    'axcye',
+    'wvxyz',
+]
+
+TEST_INPUT_02_RESULTS = {
+    ('abcde', 'axcye'): 2,
+    ('fghij', 'fguij'): 1
+}
+
+
+@pytest.mark.parametrize("input_", itertools.combinations(TEST_INPUT_02_DATA, 2))
+def test_02_count_number_of_different_letters(input_):
+    a, b = input_
+
+    try:
+        expected = TEST_INPUT_02_RESULTS[(a, b)]
+    except KeyError:
+        pass
+    else:
+        actual = aoc.day_02.number_of_different_letters(a, b)
+        assert actual == expected
+
+
+def test_02_find_letters_in_common():
+    expected = 'fgij'
+    actual = aoc.day_02.find_letters_in_common('fghij', 'fguij')
+
+    assert actual == expected
